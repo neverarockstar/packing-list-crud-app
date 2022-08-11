1b8388e34dfe20a84ee3a89f75d468a8fc86262e
neverarockstar <97563160+neverarockstar@users.noreply.github.com>
2022-08-11 19:01:51 -0400

    fixed ejs error and added delete method

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const mongoose = require('mongoose');
-require('dotenv').config();
 const PackingList = require('./models/packing-list');
+require('dotenv').config();
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect(process.env.DB_CONNECTION,
+mongoose.connect(
+    process.env.DB_CONNECTION,
     {useNewUrlParser: true},
     () => {console.log('Connected to db!')}
 )
@@ -36,39 +38,51 @@ app.post('/', async (req,res)=> {
         }
     )
     try{
-        await packingItem.save()
+        await packingItem.save();
         console.log(packingItem)
-        res.redirect('/')
+        res.redirect('/');
     }catch(err){
         if (err) return res.status(500).send(err);
         res.redirect('/')
     }
 })
 
-//EDIT (UPDATE)
+// EDIT (UPDATE)
 app
     .route('/edit/:id')
     .get((req,res) => {
         const id = req.params.id
         PackingList.find({}, (err, items) => {
-            res.render('edit.ejs', {
-                packingList: items, idItem: id })
-            })
-        })
+            res.render('edit.ejs', {packingList: items, idItem: id })
+        });
+    })
     .post((req,res) => {
-        const id = req.params.id
+        const id = req.params.id;
         PackingList.findByIdAndUpdate(
             id,
             {
                 title: req.body.title,
                 description: req.body.description
             },
+
             err => {
-                if (err) return res.status(500).send(err)
-                res.redirect('/')
-            }
-        )
-    })
+                if (err) return res.status(500).send(err);
+                res.redirect('/');
+            });
+});
+
+//DELETE
+app
+    .route('/remove/:id')
+    .get((req,res) => 
+    {
+        const id = req.params.id
+        PackingList.findByIdAndDelete(id, err => {
+            if (err) return res.status(500).send(err);
+            res.redirect('/');
+        })
+    });
+
     
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
